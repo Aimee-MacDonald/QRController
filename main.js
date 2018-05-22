0959f0e340bbb7b88c5ea23c998bde7092abfee5
Aimée <admin@aimeemacdonald.com>
2018-05-22 04:02:29 +0100

    Code Validation

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const ipc = electron.ipcMain;
+
 const path = require('path');
 const url = require('url');
+const fs = require("fs");
 
 let mainWindow;
+let cte_1_a_7;
+let cte_8_a_9;
 
 app.on("ready", () => {
   mainWindow = new BrowserWindow({width: 800, height: 600});
@@ -80,6 +84,8 @@ ipc.on("testScan", (event, args) => {
 ipc.on("testDatabase", (event, args) => {
   if(args === "back"){
     setWindow("testing");
+  } else {
+    event.sender.send("scan-validated", validateCode(args));
   }
 });
 
@@ -89,6 +95,59 @@ ipc.on("testRelays", (event, args) => {
   }
 });
 
+function validateCode(code){
+  let codeData = {
+    "valid": true,
+    "msgtexto_aid": "",
+    "channel": 0,
+    "error": 0
+  }
+
+  if(!cte_1_a_7 || !cte_8_a_9){
+    codeData.valid = false;
+    codeData.msgtexto_aid = "No configuration data found";
+    return codeData;
+  }
+
+  // err 1: Check DB
+
+  // err 2: Between 15 and 100
+  if(code.length < 15 || code.length > 100){
+    codeData.valid = false;
+    codeData.msgtexto_aid = "Invalid Length";
+    codeData.error = 2;
+    return codeData;
+  }
+
+  // err 3: 1 to 7 = "NLE0000" (cte_1_a_7)
+  if(code.substring(0, 7) !== cte_1_a_7){
+    codeData.valid = false;
+    codeData.msgtexto_aid = "Invalid \'cte_1_a_7\'";
+    codeData.error = 3;
+    return codeData;
+  }
+
+  // err 4: 8 to 9 = "01" (cte_8_a_9)
+  if(code.substring(7, 9) !== cte_8_a_9){
+    codeData.valid = false;
+    codeData.msgtexto_aid = "Invalid \'cte_8_a_9\'";
+    codeData.error = 4;
+    return codeData;
+  }
+
+  // err 5: 14 to 15 = Relay Channel (01 to 04)
+  let channel = code.substring(13, 15);
+  console.log(channel);
+  if(channel !== "01" &&
+     channel !== "02" &&
+     channel !== "03" &&
+     channel !== "04"){
+       codeData.valid = false;
+       codeData.msgtexto_aid = "Invalid Channel",
+       codeData.error = 5;
+       return codeData;
+  }
+}
 
 function setWindow(windowName){
   mainWindow.loadURL(url.format({
--- views/scripts/testDatabase.js
+++ views/scripts/testDatabase.js
@@ -4,3 +4,27 @@ const ipc = electron.ipcRenderer;
 function back(){
   ipc.send("testDatabase", "back");
 }
+
+document.addEventListener("click", () => {
+  document.getElementById("scanner-input").focus();
+});
+
+document.getElementById("input-form").addEventListener("submit", (e) => {
+  if(e.preventDefault) e.preventDefault();
+
+  let scannedCode = document.getElementById("scanner-input").value;
+
+  ipc.send("testDatabase", scannedCode);
+
+  return false;
+});
+
+ipc.on("scan-validated", function(event, args){
+  document.getElementById("code-validation").value = "";
+
+  for (var key in args) {
+    if (args.hasOwnProperty(key)) {
+      document.getElementById("code-validation").value += "\n" + (key + ": " + args[key]);
+    }
+  }
+});
